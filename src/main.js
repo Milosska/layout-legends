e36e8ddbdbac4ca561d5efb5dd265c30f733d880
Dudka Pavlo <dudka.p@outlook.com>
2026-03-26 09:14:07 +0100

    Scroll to top button

--- src/main.js
+++ src/main.js
@@ -31,3 +31,20 @@ handleWindowOpen({
   btnCloseSelector: 'data-menu-close', // data attribute for the menu close button
   backdropSelector: 'data-menu', // data attribute for the menu backdrop
 });
+
+const scrollTopBtn = document.querySelector('.js-scroll-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add('is-shown');
+  } else {
+    scrollTopBtn.classList.remove('is-shown');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
